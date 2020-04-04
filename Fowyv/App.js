e25246cc11b50eb08b87a0f83cd763222f175df1
Bernardo <39100@alunos.isel.ipl.pt>
2020-04-04 18:26:57 +0100

    Changed styles. Added Nesting Satck.Clean app.js

--- Fowyv/App.js
+++ Fowyv/App.js
@@ -1,85 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {StatusBar} from 'react-native';
 
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
 import {Home} from './app/routes/Home.js';
+import {Login} from './app/routes/Login.js';
 
-const Stack = createStackNavigator();
+const MainStack = createStackNavigator();
+const RootStack = createStackNavigator();
 
-function App() {
+function MainStackScreen() {
   return (
-    <NavigationContainer>
-      {
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      }
-    </NavigationContainer>
+    <MainStack.Navigator screenOptions={{headerShown: false}}>
+      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen name="Login" component={Login} />
+    </MainStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+export class App extends React.Component {
+  componentDidMount() {
+    StatusBar.setBarStyle('light-content', true);
+    StatusBar.setBackgroundColor('orange');
+  }
+  render() {
+    return (
+      <NavigationContainer>
+        {
+          <RootStack.Navigator screenOptions={{headerShown: false}}>
+            <RootStack.Screen name="Main" component={MainStackScreen} />
+          </RootStack.Navigator>
+        }
+      </NavigationContainer>
+    );
+  }
+}
 
 export default App;
--- Fowyv/app/routes/Home.js
+++ Fowyv/app/routes/Home.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, Text, View, StyleSheet} from 'react-native';
 
 export class Home extends React.Component {
-
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', justifyContent: 'center',backgroundColor: 'orange'}}>
-        <Image style={{ width: 180, height: 180 , resizeMode: 'contain' }} source={require('../assets/images/logo.png')} />
-        <Text style={{ fontSize:50, fontWeight:'bold', color:'white'}}>FOWYV</Text>
-        <Text style={{fontSize:20,}}>find others with your voice</Text>
+      <View style={homeStyle.View}>
+        <Image
+          style={homeStyle.Logo}
+          source={require('../assets/images/logo.png')}
+        />
+        <Text style={homeStyle.Name}>FOWYV</Text>
+        <Text style={homeStyle.Description}>find others with your voice</Text>
       </View>
     );
   }
 }
+
+const homeStyle = StyleSheet.create({
+  View: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'orange',
+  },
+  Logo: {width: 180, height: 180, resizeMode: 'contain'},
+  Name: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  Description: {fontSize: 20},
+});
