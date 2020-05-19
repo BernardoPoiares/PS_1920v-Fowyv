f3fb20b98d001af042ee9da86a90b4a194ff5bb1
Bernardo <39100@alunos.isel.ipl.pt>
2020-05-19 01:04:24 +0100

    Get email on token verification

--- Fowyv_Backend/middlewares/authMiddleware.js
+++ Fowyv_Backend/middlewares/authMiddleware.js
@@ -12,7 +12,7 @@ verifyToken = (req, res, next) => {
     if (err) {
       return res.status(401).send({ message: "Unauthorized!" });
     }
-    req.userId = decoded.id;
+    req.email = decoded.email;
     next();
   });
 };
